66e9d0f4f757141aa5e32811cc08ace037b08cff
thechutrain <chu.chuyalan@gmail.com>
2018-09-20 13:21:40 -0400

    added dashed border and clean up

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -20,24 +20,6 @@ function paint() {
 	// Draw on the canvas:
 	ctx = bgCanvas.getContext('2d');
 
-	//#region Rectangle example 1
-	// ctx.fillStyle = 'rgb(200, 0, 0, 0.5)';
-	// ctx.fillRect(10, 10, 50, 50);
-
-	// ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-	// ctx.fillRect(30, 30, 50, 50);
-
-	// ctx.fillRect(25, 50, 100, 200);
-	//#endregion
-
-	//#region triangle
-	// ctx.beginPath();
-	// ctx.moveTo(75, 50);
-	// ctx.lineTo(100, 75);
-	// ctx.lineTo(100, 25);
-	// ctx.fill();
-	//#endregion
-
 	//#region circl
 	ctx.beginPath();
 	ctx.arc(200, 200, 3, 0, Math.PI * 2, true);
@@ -46,4 +28,10 @@ function paint() {
 	//#endregion
 }
 
+// window.main = function reqAnimationFrame() {
+// 	window.requestAnimationFrame(main);
+
+// 	console.log('requested!');
+// };
+// main();
 module.exports = exports = paint;
