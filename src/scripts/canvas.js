7986336d839987b26735ebef449af33dc0eee248
thechutrain <chu.chuyalan@gmail.com>
2018-09-20 14:14:30 -0400

    got something to animate by using translate ...

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,37 +1,60 @@
 'use strict';
 
+// this --> points to canvas element
+let count = 0;
 function paint() {
-	console.log(`painting canvas ...`);
+	// DEBUGGING;
+	count++;
+	if (count > 100) {
+		console.log(`hit 100times`);
+		count = count - 100;
+	}
+
+	// Draw on the canvas:
+	const ctx = this.getContext('2d');
+
+	//#region circle
+	ctx.beginPath();
+	ctx.arc(200, 200, 6, 0, Math.PI * 2, true);
+	ctx.stroke();
+	ctx.fill();
+	ctx.save();
+	ctx.translate(2, 1);
+	//#endregion
+
+	window.requestAnimationFrame(paint.bind(this));
+}
+
+function init() {
+	const bgCanvas = validateCanvas();
+	if (!bgCanvas) return;
+
+	window.requestAnimationFrame(paint.bind(bgCanvas));
+}
+
+/** initializes canvas element & checks that its in the dom
+ *
+ * @return bgCanvas {canvas Element}
+ */
+//#region
+function validateCanvas() {
 	const bgCanvas = document.getElementById('bg-canvas');
-	let ctx;
+	// let ctx;
 
 	// Check for compatibility:
 	if (!bgCanvas) {
-		return;
+		return false;
 	} else if (!bgCanvas.getContext) {
 		console.warn('canvas getContext not supported!');
-		return;
+		return false;
 	}
 
 	// Resize canvas:
 	bgCanvas.width = window.innerWidth;
 	bgCanvas.height = window.innerHeight;
 
-	// Draw on the canvas:
-	ctx = bgCanvas.getContext('2d');
-
-	//#region circl
-	ctx.beginPath();
-	ctx.arc(200, 200, 3, 0, Math.PI * 2, true);
-	ctx.stroke();
-	ctx.fill();
-	//#endregion
+	return bgCanvas;
 }
+//#endregion
 
-// window.main = function reqAnimationFrame() {
-// 	window.requestAnimationFrame(main);
-
-// 	console.log('requested!');
-// };
-// main();
-module.exports = exports = paint;
+module.exports = exports = init;
