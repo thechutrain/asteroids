07b272ff37263d944a748f5d88fe106d7c1f21cd
thechutrain <chu.chuyalan@gmail.com>
2018-09-20 16:11:42 -0400

    added in the tick check but not smooth

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,13 +1,28 @@
 'use strict';
 
-// this --> points to canvas element
 let count = 0;
 let prevTime = window.performance.now();
+let frameTime = 20;
 
 function paint(highResTimestamp) {
+	// RequestAnimationFrame as first line, good practice as per se MDN
 	window.requestAnimationFrame(paint.bind(this));
+
 	// Draw on the canvas:
 	const ctx = this.getContext('2d');
+
+	// TODO: determine the number of ticks since last render
+	// const timeSinceUpdate = highResTimestamp - prevTime;
+	// let ticks = 0;
+
+	// if (timeSinceUpdate > frameTime) {
+	// 	ticks = Math.floor(timeSinceUpdate / frameTime);
+	// 	prevTime = window.performance.now(); // or should i do highResTimestamp?
+	// }
+
+	// if (ticks === 0) {
+	// 	return;
+	// }
 
 	//#region circle
 	// ctx.save();
@@ -18,23 +33,20 @@ function paint(highResTimestamp) {
 	//#endregion
 
 	ctx.clearRect(0, 0, 300, 300); // clear rectangle
+	// ctx.restore();
 
 	// make a box
 	ctx.fillRect(5, 10, 50, 50);
 	ctx.fillStyle = 'rgba(48, 128, 232, 0.6)';
-	ctx.translate(0.5, 2);
-
-	// ctx.save();
-	// ctx.restore();
-
-	// ctx.translate(2, 1);
+	// ctx.translate(0.5 * ticks, 2 * ticks);
+	ctx.translate(0.5, 1);
+	ctx.save();
 
 	// DEBUGGING PURPOSES:
 	let timePastUpdate = highResTimestamp - prevTime;
 	if (timePastUpdate > 20) {
 		console.log(timePastUpdate);
 	}
-
 	prevTime = highResTimestamp;
 }
 
