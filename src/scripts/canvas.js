2edf4925ec3afca78a978b1964319bf2db657b0e
thechutrain <chu.chuyalan@gmail.com>
2018-09-20 15:01:35 -0400

    moving box yeah!

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -2,29 +2,43 @@
 
 // this --> points to canvas element
 let count = 0;
-function paint() {
-	// DEBUGGING;
-	count++;
-	if (count > 100) {
-		console.log(`hit 100times`);
-		count = count - 100;
-	}
+let prevTime = window.performance.now();
 
+function paint(highResTimestamp) {
+	window.requestAnimationFrame(paint.bind(this));
 	// Draw on the canvas:
 	const ctx = this.getContext('2d');
 
 	//#region circle
-	ctx.beginPath();
-	ctx.arc(200, 200, 6, 0, Math.PI * 2, true);
-	ctx.stroke();
-	ctx.fill();
-	ctx.save();
-	ctx.translate(2, 1);
+	// ctx.save();
+	// ctx.beginPath();
+	// ctx.arc(200, 200, 6, 0, Math.PI * 2, true);
+	// ctx.stroke();
+	// ctx.fill();
 	//#endregion
 
-	window.requestAnimationFrame(paint.bind(this));
+	ctx.clearRect(0, 0, 300, 300); // clear rectangle
+
+	// make a box
+	ctx.fillRect(5, 10, 50, 50);
+	ctx.fillStyle = 'rgba(48, 128, 232, 0.6)';
+	ctx.translate(0.5, 2);
+
+	// ctx.save();
+	// ctx.restore();
+
+	// ctx.translate(2, 1);
+
+	// DEBUGGING PURPOSES:
+	let timePastUpdate = highResTimestamp - prevTime;
+	if (timePastUpdate > 20) {
+		console.log(timePastUpdate);
+	}
+
+	prevTime = highResTimestamp;
 }
 
+// ================= initialization stuff ===============
 function init() {
 	const bgCanvas = validateCanvas();
 	if (!bgCanvas) return;
