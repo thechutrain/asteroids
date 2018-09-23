4f538944c58bb71fe79b36714b394f7dd34b69b9
thechutrain <chu.chuyalan@gmail.com>
2018-09-23 10:43:55 -0400

    wrote a test for the asteroids

--- src/scripts/canvas/__test__/Asteroid.test.js
+++ src/scripts/canvas/__test__/Asteroid.test.js
@@ -2,13 +2,21 @@
 
 const Asteroid = require('../Asteroid');
 
+const fakeCanvasElem = {
+	width: 200,
+	height: 100,
+};
+
 describe('test', () => {
-	it('should pass', () => {
-		expect('b').toBe('b');
+	it('should have a random speed', () => {
+		const a = new Asteroid({ canvasElem: fakeCanvasElem, ctx: null });
+		expect(a.options.translateX).not.toBe(-2);
 	});
 
-	it('should have a random speed', () => {
-		const a = new Asteroid({ canvasElem: null, ctx: null });
-		expect(a.options.translateX).not.toBe(-2);
+	it('should be able to determine if its completelly visible', () => {
+		const a = new Asteroid({ canvasElem: fakeCanvasElem, ctx: null });
+		a.points = [{ x: 30, y: 20 }, { x: 40, y: 30 }, { x: 30, y: 40 }];
+
+		expect(a.isVisible()).toBe(true);
 	});
 });
